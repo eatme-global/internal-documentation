096f6879f3b5794983a2d2f10968b0258a1b97e5
Sivantha Paranavithana <sivantha.p@eatmeglobal.com>
2024-08-02 11:14:08 +0530

    refactor: code improvements

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   organizationName: "eatme-global",
   projectName: "internal-documentation",
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   i18n: {
